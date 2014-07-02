c2c77615480af8deed894fda011101f120653e2d
Garret Raziel <boloomka@gmail.com>
2014-07-02 22:38:09 +0200

    add socket.io, placeholder express app

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
+/**
+ * @file Main server file for QB project.
+ * @author Jan Sedlak
+ */
+
+// std modules
 var http = require("http");
 var https = require("https");
+
+// installed modules
+var express = require("express");
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello, world!');
+});
+
+app.listen(3000);
