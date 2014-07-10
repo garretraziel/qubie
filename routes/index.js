45e96a5ff4f18f8335defbb431ed37255c95385b
Garret Raziel <boloomka@gmail.com>
2014-07-10 14:00:45 +0200

    work on page style

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ var router = express.Router();
 router.get('/', function (req, res) {
     res.render('index');
 });
+router.get('/login', function (req, res) {
+    res.render('login');
+});
 
 module.exports = router;
