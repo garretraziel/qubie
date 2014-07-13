ffd8f2daed79393fdf17356ab1dae452fdf2e766
garretraziel <boloomka@gmail.com>
2014-07-13 23:15:30 +0200

    beginning of file uploading

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,31 @@
 var express = require('express');
+var AWS = require('aws-sdk');
+var multiparty = require('multiparty');
 
 module.exports = function (db, memstore) {
     var router = express.Router();
 
-    router.get('/', function (req, res) {
+    router.use(function (req, res, next) {
         if (req.isAuthenticated()) {
-            res.render('user', {username: req.user.username});
+            next();
         } else {
-            res.redirect('/login');
+            res.redirect('/login'); // TODO: flash a navrat zpet
         }
+    });
+
+    router.get('/', function (req, res) {
+        res.render('user', {username: req.user.username});
+    });
+    router.get('/upload', function (req, res) {
+        res.render('user/upload');
+    });
+    router.post('/upload', function (req, res) {
+        var form = new multiparty.Form(); // TODO: asi nejdriv nejaka bezpecnost
+
+        form.parse(req, function (err, fields, files) {
+            // TODO: upload do aws, cteni/zapis do databaze
+            res.redirect('/upload');
+        });
     });
 
     return router;
