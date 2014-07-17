6677ea575059edee6d5d2e62992685d4a2f48cf1
garretraziel <boloomka@gmail.com>
2014-07-17 22:42:24 +0200

    simple s3 upload

--- routes/user.js
+++ routes/user.js
@@ -23,15 +23,45 @@ module.exports = function (config, db, memstore) {
     });
     router.post('/upload', function (req, res) {
         var form = new multiparty.Form(); // TODO: asi nejdriv nejaka bezpecnost
+        // TODO: uklizet soubory
+        var s3bucket = new AWS.S3({params: {Bucket: config.aws_bucket}});
+        // TODO: upload do aws, cteni/zapis do databaze
 
-        form.parse(req, function (err, fields, files) {
-            // TODO: upload do aws, cteni/zapis do databaze
+        form.on('part', function (part) {
             var id = shortId.generate();
-            var out = fs.createWriteStream("out.out");
-            files['file'][0].createReadStream().pipe(out);
 
+            if (part.filename === null) {
+                console.log("Got unhandled field: " + part.name);
+                part.resume();
+            }
+
+            if (part.filename !== null) {
+                part.length = part.byteCount; // TODO: jak je to presne s velikosti?
+                s3bucket.putObject({
+                    Key: id,
+                    Body: part,
+                    ACL: 'public-read' // TODO: asi nejake signed url
+                }, function (err, data) {
+                    if (err) {
+                        console.error("ERR S3:", err); // TODO: resit nejak vic
+                    } else {
+                        console.log(data);
+                        db.Document.create({
+                            // TODO: url ziskat nejak rozumnejic
+                            url: "https://" + config.aws_bucket + ".s3.amazonaws.com/" + id
+                        }).success(function (document) {
+                            document.setUser(req.user);
+                        });
+                    }
+                });
+            }
+        });
+
+        form.on('close', function () {
             res.redirect('back');
         });
+
+        form.parse(req);
     });
 
     return router;
