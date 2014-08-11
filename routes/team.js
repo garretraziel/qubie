7b5c80f25dc946fcbc3f5fa27263f815d2a3616e
garretraziel <boloomka@gmail.com>
2014-08-11 21:42:58 +0200

    changes in updating user info in admin, add basic team router

---
+++ routes/team.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+module.exports = function (config, db, memstore) {
+    var router = express.Router();
+
+    router.use(function (req, res, next) {
+        if (req.isAuthenticated()) {
+            next();
+        } else {
+            res.redirect('/login'); // TODO: flash a navrat zpet
+        }
+    });
+
+    return router;
+};
