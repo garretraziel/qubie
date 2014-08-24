40dc9aa21ab99bb3c16fe068bb8e5537d9a0e91c
garretraziel <boloomka@gmail.com>
2014-08-24 22:16:49 +0200

    auth in routes/document, pdf for anonymous user

--- routes/document.js
+++ routes/document.js
@@ -17,30 +17,38 @@ module.exports = function (config, db, memstore) {
     });
 
     router.get('/:document_id', function (req, res) {
-        db.Document.find(req.params.document_id).success(function (document) {
-            document.getUser().success(function (result) {
-                if (result.id === req.user.id) {
-                    res.render('document/root', {
-                        ID: req.params.document_id,
-                        name: document.name
-                    });
-                } else {
-                    res.redirect('/fail');
-                }
+        if (req.isAuthenticated()) {
+            db.Document.find(req.params.document_id).success(function (document) {
+                document.getUser().success(function (result) {
+                    if (result.id === req.user.id) {
+                        res.render('document/root', {
+                            ID: req.params.document_id,
+                            name: document.name
+                        });
+                    } else {
+                        res.redirect('/fail');
+                    }
+                });
             });
-        });
+        } else {
+            res.redirect('/login');
+        }
     });
     router.get('/:document_id/document', function (req, res) {
-        db.Document.find(req.params.document_id).success(function (document) {
-            document.getUser().success(function (result) {
-                if (result.id === req.user.id) {
-                    var params = {Key: document.key};
-                    s3bucket.getObject(params).createReadStream().pipe(res);
-                } else {
-                    res.redirect('/fail');
-                }
+        if (req.isAuthenticated()) {
+            db.Document.find(req.params.document_id).success(function (document) {
+                document.getUser().success(function (result) {
+                    if (result.id === req.user.id) {
+                        var params = {Key: document.key};
+                        s3bucket.getObject(params).createReadStream().pipe(res);
+                    } else {
+                        res.redirect('/fail');
+                    }
+                });
             });
-        });
+        } else {
+            res.redirect('/login');
+        }
     });
     router.get('/p/:presenter_id', function (req, res) {
         memdb.getPresenterId(memstore, req.params.presenter_id, function (id) {
@@ -49,6 +57,18 @@ module.exports = function (config, db, memstore) {
             } else {
                 res.render('document/presenter', {
                     presenter_id: req.params.presenter_id
+                });
+            }
+        });
+    });
+    router.get('/d/:presenter_id', function (req, res) {
+        memdb.getPresenterId(memstore, req.params.presenter_id, function (id) {
+            if (id === false) {
+                res.redirect('/fail');
+            } else {
+                db.Document.find(id).success(function (document) {
+                    var params = {Key: document.key};
+                    s3bucket.getObject(params).createReadStream().pipe(res);
                 });
             }
         });
